2096ed7c83c2ac93ab89bff1aabfe32d7448314f
marz-freeman <marzfreeman21@gmail.com>
2025-01-02 12:02:12 -0400

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/marz-freeman.py
@@ -0,0 +1,16 @@
+# https://www.python.org/
+
+#comentario en una line
+
+"""
+esto es tambien 
+es un comentario
+en varias lineas
+"""
+
+
+my_variable = "mi variable"
+my_variable = "nuevo valor de mi variable"
+
+MY_CONSTANT = "mi constante " #por covencion
+
